7c35caf5b422a9cdb0083238df3ccd82b4907788
David Burke <dburke@amberfire.net>
2023-12-04 11:15:21 -0500

    Day 4, Part 2 complete

--- day04/day04.py
+++ day04/day04.py
@@ -1,20 +1,42 @@
 import sys
-
+from os.path import exists
 
 def read_input(file_name):
     with open(file_name, 'r') as file:
         data = file.readlines()
     return data 
 
-def process_input(data, runt_type) -> int:
+def is_args_valid() -> bool:
+    if len(sys.argv) < 3:
+        print("Must specify run type and input file")
+        return False
+    if "AB".find(sys.argv[1]) < 0:
+        print("First parameter is the run type and must be 'A' or 'B")
+        return False
+    if not exists(sys.argv[2]):
+        print(f"File {sys.argv[2]} does not exist")
+        return False
+    
+    return True
+
+def process_input(data, run_type) -> int:
     answer = 0
-    game = 0
+    game = -1
+    if run_type == "B":
+        scores = [0] * len(data)
+
     for line in data:
         game += 1
         winning_numbers = get_numbers(line, True)
-        score = get_score(line, winning_numbers)
-        print(f"Game {game}: {score}")
-        answer += score
+        score = get_score(line, winning_numbers, run_type)
+        if run_type == "A":
+            answer += score
+        else:
+            scores[game] = score
+
+    if run_type == "B":
+        answer = process_scores(scores)
+
     return answer
 
 def get_numbers(line, is_winning) -> list:
@@ -35,17 +57,38 @@ def get_numbers(line, is_winning) -> list:
         start  += 3
     return numbers
 
-def get_score(line, winning_numbers) -> int:
+def get_score(line, winning_numbers, run_type) -> int:
     score = 0
     numbers = get_numbers(line,False)
     for num in numbers:
         if num in winning_numbers:
-            if score == 0:
-                score = 1
+            if run_type == "A":
+                if score == 0:
+                    score = 1
+                else:
+                    score = score * 2
             else:
-                score = score * 2
+                score += 1
+
     return score
     
+def process_scores(scores)->int:
+    answer = len(scores)
+    for card in range(0, len(scores)):
+        answer += process_card(scores, card)
+    return answer    
+
+def process_card(scores, card)->int:
+    
+    answer = scores[card]
+    if answer > 0:
+        for i in range(card+1,card+1+answer):
+            answer += process_card(scores, i)
+    return answer
+        
+if not is_args_valid():
+    print("Unrecoverable error - terminating program")
+    sys.exit()
 
 data = read_input(sys.argv[2])
 answer = process_input(data, sys.argv[1])
