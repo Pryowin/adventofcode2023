d26a2774c96223e9f8f7f1ba3598e2044c6d7bec
David Burke <dburke@amberfire.net>
2023-12-04 09:51:05 -0500

    Day 4, Part refactor complete

--- day04/day04.py
+++ day04/day04.py
@@ -11,25 +11,24 @@ def process_input(data, runt_type) -> int:
     game = 0
     for line in data:
         game += 1
-        winning_numbers = get_winning_numbers(line)
+        winning_numbers = get_numbers(line, True)
         score = get_score(line, winning_numbers)
         print(f"Game {game}: {score}")
         answer += score
     return answer
 
-def get_winning_numbers(line) -> list:
-    winning_numbers = []
-    start = line.find(":") + 2
-    while line[start] != "|":
-        num = int(line[start:start+2])
-        winning_numbers.append(num)
-        start  += 3
-    return winning_numbers
+def get_numbers(line, is_winning) -> list:
 
-def get_numbers(line) -> list:
-    start = line.find("|") + 2
-    iterations = int(len(line[start:])/3)
     numbers = []
+
+    if is_winning:
+        start = line.find(":") + 2
+        end = line.find("|")
+        iterations = int(len(line[start:end])/3)
+    else:
+        start = line.find("|") + 2
+        iterations = int(len(line[start:])/3)
+
     for i in range(0,iterations):
         num = int(line[start:start+2])
         numbers.append(num)
@@ -38,7 +37,7 @@ def get_numbers(line) -> list:
 
 def get_score(line, winning_numbers) -> int:
     score = 0
-    numbers = get_numbers(line)
+    numbers = get_numbers(line,False)
     for num in numbers:
         if num in winning_numbers:
             if score == 0:
