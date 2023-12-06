aff603a9a20d0d5aef9537a586e89f45e64d94b5
David Burke <dburke@amberfire.net>
2023-12-06 09:55:19 -0500

    Updated template to work correctly with pytest

--- python_modules/advent_template.py
+++ python_modules/advent_template.py
@@ -6,11 +6,11 @@ import advent
 def process_input(data, run_type) -> int:
     return 0
 
+if __name__ == '__main__':
+    if not advent.is_args_valid(sys.argv):
+        print("Unrecoverable error - terminating program")
+        sys.exit()
 
-if not advent.is_args_valid(sys.argv):
-    print("Unrecoverable error - terminating program")
-    sys.exit()
-
-data = advent.read_input(sys.argv[2])
-answer = process_input(data, sys.argv[1])
-print(f"Answer : {answer}")
+    data = advent.read_input(sys.argv[2])
+    answer = process_input(data, sys.argv[1])
+    print(f"Answer : {answer}")
