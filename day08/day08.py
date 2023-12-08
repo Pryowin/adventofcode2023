9c9c19623d4880c56fa095a85300200952e23789
David Burke <dburke@amberfire.net>
2023-12-08 14:20:48 -0500

    Day 8 Part 2 done

--- day08/day08.py
+++ day08/day08.py
@@ -1,4 +1,6 @@
 import sys
+import math
+from functools import reduce
 
 sys.path.append( '../python_modules' )
 import advent
@@ -6,10 +8,45 @@ import advent
 def process_input(data, run_type) -> int:
     instructions = get_instructions(data[0])
     location_dictionary = get_location_dict(data[2:])
-    start = "AAA"
-    destination = "ZZZ"
+    if run_type == "A":
+        start = "AAA"
+        destination = "ZZZ"
+        answer = navigate(location_dictionary, instructions, start, destination)
+    else:
+        start_list = get_start_locations(location_dictionary)
+        answer = ghost_move(location_dictionary,instructions, start_list)
+    
+    return answer
 
-    return navigate(location_dictionary, instructions, start, destination, run_type)
+def get_start_locations(location_dictionary):
+    return list(filter(lambda str: str[2] == "A", location_dictionary))
+
+def count_destinations(location_dictionary) -> int:
+    return len(list(filter(lambda str: str[2] == "Z", location_dictionary)))
+
+def ghost_move(location_dictionary, instructions, start_list) -> int:
+    
+    ghost_number = len(start_list)
+    moves_to_z = [0] * ghost_number
+    instruction_count = len(instructions)
+    for idx in range(0, len(start_list)):
+        current_location = start_list[idx]
+        moves = - 1
+        while current_location[2] != "Z":
+            moves +=1 
+            instruction = instructions[moves % instruction_count]
+            node = location_dictionary[current_location]
+            current_location = get_next_node(node,instruction)
+        
+        moves_to_z[idx] = moves + 1
+
+
+    return lcm(moves_to_z)
+    
+def lcm(arr):
+
+    l=reduce(lambda x,y:(x*y)//math.gcd(x,y),arr)
+    return l
 
 def get_instructions(line):
     return list(line.strip())
@@ -39,7 +76,7 @@ def get_next_node(current_node, instruction):
     else:
         return current_node[1]
     
-def navigate(location_dictionary, instructions, start, destination, run_type) -> int:
+def navigate(location_dictionary, instructions, start, destination) -> int:
     count = -1
     current_location = start
     instruction_count = len(instructions)
--- day08/test_day08.py
+++ day08/test_day08.py
@@ -8,3 +8,9 @@ def test_get_left_right():
 
 def test_get_location_dict():
     assert day08.get_location_dict(["AAA = (BBB, BBB)", "BBB = (AAA, CCC)"]) == {'AAA': ['BBB','BBB'], 'BBB': ['AAA', 'CCC'] }
+
+def test_get_start_locations():
+    assert day08.get_start_locations({'AAA': ['BBB','BBB'], 'BBB': ['AAA', 'CCC'], 'ZYA': ['AAA', 'CCC']} ) == ['AAA', "ZYA"]
+
+def test_count_destinations():
+    assert day08.count_destinations({'AAA': ['BBB','BBB'], 'BBZ': ['AAA', 'CCC'], 'ZYA': ['AAA', 'CCC']}) == 1
