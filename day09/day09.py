69c1ac881c5a83341100bd5a9816da5544c7798e
David Burke <dburke@amberfire.net>
2023-12-09 09:31:46 -0500

    Day 9, Part 1 Done

---
+++ day09/day09.py
@@ -0,0 +1,55 @@
+import sys
+
+sys.path.append( '../python_modules' )
+import advent
+
+def process_input(data, run_type) -> int:
+    oasis = get_oasis(data)
+    answer = 0
+    for list in oasis:
+        answer += make_predictions(get_sequences(list))
+    
+    return answer
+
+def get_oasis(data):
+    ret_val =[]
+    for line in data:
+        ret_val.append(advent.get_numbers(line,0))
+    return ret_val
+
+def get_differences(num_list):
+    ret_val = []
+    for i in range(1,len(num_list)):
+        ret_val.append(num_list[i]-num_list[i-1])
+    return ret_val
+
+def is_all_zero(num_list):
+    return num_list == [0] * len(num_list)
+
+def get_sequences(num_list):
+    sequences = []
+    idx = 0
+    sequences.append(num_list)
+    while not is_all_zero(sequences[idx]):
+        sequences.append(get_differences(sequences[idx]))
+        idx +=1
+    
+    return sequences
+
+def make_predictions(num_list) -> int:
+    idx = len(num_list)
+    num = 0
+    while idx > 0:
+        idx -= 1
+        num += num_list[idx][-1]
+    
+    return num 
+
+if __name__ == '__main__':
+    if not advent.is_args_valid(sys.argv):
+        print("Unrecoverable error - terminating program")
+        sys.exit()
+
+    data = advent.read_input(sys.argv[2])
+    answer = process_input(data, sys.argv[1])
+    print(f"Answer : {answer}")
---
+++ day09/test_day09.py
@@ -0,0 +1,25 @@
+import day09
+
+def test_get_oasis():
+    arr = ["-7 -13 -20 -17 16",
+           "0 3 6 9 12 15"
+           ]
+    assert day09.get_oasis(arr) == [[-7,-13,-20,-17,16],[0,3,6,9,12,15]]
+
+def test_get_differences():
+    assert day09.get_differences([0, 3, 6, 9, 12, 15,]) == [3, 3, 3, 3, 3]
+
+def test_is_all_zero():
+    assert day09.is_all_zero([0, 0, 0, 0]) == True
+
+def test_is_all_zero_not():
+    assert day09.is_all_zero([3, 3, 3, 3, 3]) == False
+
+def test_get_sequences():
+    assert day09.get_sequences([0, 3, 6, 9, 12, 15]) == [[0, 3, 6, 9, 12, 15],
+                                                        [3, 3, 3, 3, 3],
+                                                        [0, 0, 0, 0]]
+    
+def test_make_predictions():
+    seq = day09.get_sequences([0, 3, 6, 9, 12, 15])
+    assert day09.make_predictions(seq) == 18
